82301aa99fc31eddcabc44b84c2e37e9c45fdbef
unknown <jerusarutazamba@gmail>
2022-04-24 23:45:54 +0300

    my msg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
-                " seconds is " + finalPosition + " m.");
+        double a = -9.81; // Earth's gravity in m/s^2
+        double Vi = 0.0;
+        double Xi = 0.0;
+        double t = 10.0;
+        double  x;
+
+        x = 0.5* a*(t*t) + (Vi*t) + Xi;
+
+        System.out.println("The object's position after " + t +
+                " seconds is " + x + " m.");
     }
 }
